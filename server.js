d4741519509e90a5e7a4b7f3afd4ef05b8e717b1
chelahcynthia <cchelangat989@gmail.com>
2023-09-07 19:54:14 +0300

    Installed nodemon

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Node Api')
+})
+
+app.listen(3000, () => {
+    console.log("node api is running on port 3000");
+});
