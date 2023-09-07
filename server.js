ff888c6f41867b8479fbe2ad14b4a6a984b91f69
chelahcynthia <cchelangat989@gmail.com>
2023-09-07 22:24:13 +0300

    Connected mongodb to the server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.get('/', (req, res) => {
@@ -12,4 +13,17 @@ app.get('/blog/', (req, res) => {
 app.listen(3000, () => {
     console.log("node api is running on port 3000");
 });
+
+
+mongoose.set("strictQuery", false)
+mongoose.
+connect('mongodb+srv://chelah:2379@cluster0.ube6ozj.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log(`Node API app is running on port 3000`)
+    });
+}).catch((error) => {
+    console.log(error)
+})
 
