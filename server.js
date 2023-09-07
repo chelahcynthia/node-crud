8a466a642fce65380a53dfadb00c8b7603e500e8
chelahcynthia <cchelangat989@gmail.com>
2023-09-07 19:59:32 +0300

    Added another blog route on server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
     res.send('Hello Node Api')
 })
 
+app.get('/blog/', (req, res) => {
+    res.send('Hello Blog, My name is Chelah')
+})
+
 app.listen(3000, () => {
     console.log("node api is running on port 3000");
 });
+
