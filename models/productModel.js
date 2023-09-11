29437c613c9f784a1c1a24c7e5edeabaf3a079bd
chelahcynthia <cchelangat989@gmail.com>
2023-09-11 11:28:25 +0300

    Learned how to find products by Id

--- models/productModel.js
+++ models/productModel.js
@@ -5,6 +5,28 @@ const productSchema = mongoose.Schema(
         name: {
             type: String,
             required: [true, "Please enter a product name"]
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        image:{
+            type: String,
+            required: false,
         }
+
+    },
+    {
+        timestamps: true
     }
+    
 )
+
+const Product = mongoose.model('product', productSchema);
+
+module.exports = Product;
--- server.js
+++ server.js
@@ -1,14 +1,52 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/productModel');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+// routes
 app.get('/', (req, res) => {
     res.send('Hello Node Api')
 })
 
-app.get('/blog/', (req, res) => {
+app.get('/blog', (req, res) => {
     res.send('Hello Blog, My name is Chelah')
 })
+
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+app.get('/products/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+app.post('/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
 
 app.listen(3000, () => {
     console.log("node api is running on port 3000");
